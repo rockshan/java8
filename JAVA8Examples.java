7936b72ee8cf69b26cd377cc2410fb91b9a3c92c
rockshan <jha.nitt.cse@gmail.com>
2019-03-31 16:43:03 -0700

    Update JAVA8Examples.java

--- JAVA8Examples.java
+++ JAVA8Examples.java
@@ -24,4 +24,18 @@ public class Java8CodeExamples {
                 .mapToInt(Integer::intValue)
                 .max();
     }
+    
+    
+    /**
+     * Accepts list for words and create a hashmap of string and its length.
+     * This should work for empty and null string
+     *
+     */
+    public static Map<String, Integer> getStringLengthPair(List<String> list) {
+        return Optional.ofNullable(list)
+                .orElse(Collections.emptyList())
+                .stream()
+                //.collect(Collectors.toMap(w -> w, w -> w.length())) -> this is also correct
+                .collect(Collectors.toMap(String::toString, String::length));
+    }
 }
